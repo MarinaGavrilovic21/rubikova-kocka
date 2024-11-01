1759fcd1f4b072a4823147703592724618b41e89
Marina Gavrilovic <marina.gavrilovic21.mg@gmail.com>
2024-11-01 18:10:38 +0100

    iscrtavanje kocke dodato

--- Cube.py
+++ Cube.py
@@ -1,6 +1,6 @@
-from array import array
-
 import random
+import numpy as np
+import cv2 as cv
 
 class Cube:
     def __init__(self):
@@ -204,7 +204,103 @@ class Cube:
             elif selected_function == functions[11]:
                 kocka.rotate_back_counter_clockwise()
 
-    #def draw_cube(self):
+    def draw_cube(self):
+
+        # Create a black image
+        img = np.zeros((1200, 1200, 3), np.uint8)
+
+        # Drawing rectangle
+        # Top
+        # Prva kolona
+        cv.rectangle(img, (242, 5), (316, 79), (255, 255, 255), -1)
+        cv.rectangle(img, (242, 81), (316, 155), (255, 255, 255), -1)
+        cv.rectangle(img, (242, 157), (316, 231), (255, 255, 255), -1)
+        # Druga kolona
+        cv.rectangle(img, (318, 5), (392, 79), (255, 255, 255), -1)
+        cv.rectangle(img, (318, 81), (392, 155), (255, 255, 255), -1)
+        cv.rectangle(img, (318, 157), (392, 231), (255, 255, 255), -1)
+        # Treca kolona
+        cv.rectangle(img, (394, 5), (468, 79), (255, 255, 255), -1)
+        cv.rectangle(img, (394, 81), (468, 155), (255, 255, 255), -1)
+        cv.rectangle(img, (394, 157), (468, 231), (255, 255, 255), -1)
+
+        # Front
+        # Prva kolona
+        cv.rectangle(img, (242, 247), (316, 321), (0, 0, 255), -1)
+        cv.rectangle(img, (242, 323), (316, 397), (0, 0, 255), -1)
+        cv.rectangle(img, (242, 399), (316, 473), (0, 0, 255), -1)
+        # Druga kolona
+        cv.rectangle(img, (318, 247), (392, 321), (0, 0, 255), -1)
+        cv.rectangle(img, (318, 323), (392, 397), (0, 0, 255), -1)
+        cv.rectangle(img, (318, 399), (392, 473), (0, 0, 255), -1)
+        # Treca kolona
+        cv.rectangle(img, (394, 247), (468, 321), (0, 0, 255), -1)
+        cv.rectangle(img, (394, 323), (468, 397), (0, 0, 255), -1)
+        cv.rectangle(img, (394, 399), (468, 473), (0, 0, 255), -1)
+
+        # Bottom
+        # Prva kolona
+        cv.rectangle(img, (242, 489), (316, 563), (0, 255, 255), -1)
+        cv.rectangle(img, (242, 565), (316, 639), (0, 255, 255), -1)
+        cv.rectangle(img, (242, 641), (316, 715), (0, 255, 255), -1)
+        # Druga kolona
+        cv.rectangle(img, (318, 489), (392, 563), (0, 255, 255), -1)
+        cv.rectangle(img, (318, 565), (392, 639), (0, 255, 255), -1)
+        cv.rectangle(img, (318, 641), (392, 715), (0, 255, 255), -1)
+        # Treca kolona
+        cv.rectangle(img, (394, 489), (468, 563), (0, 255, 255), -1)
+        cv.rectangle(img, (394, 565), (468, 639), (0, 255, 255), -1)
+        cv.rectangle(img, (394, 641), (468, 715), (0, 255, 255), -1)
+
+        # Left
+        # Prva kolona
+        cv.rectangle(img, (0, 247), (74, 321), (0, 200, 60), -1)
+        cv.rectangle(img, (0, 323), (74, 397), (0, 200, 60), -1)
+        cv.rectangle(img, (0, 399), (74, 473), (0, 200, 60), -1)
+        # Druga kolona
+        cv.rectangle(img, (76, 247), (150, 321), (0, 200, 60), -1)
+        cv.rectangle(img, (76, 323), (150, 397), (0, 200, 60), -1)
+        cv.rectangle(img, (76, 399), (150, 473), (0, 200, 60), -1)
+        # Treca kolona
+        cv.rectangle(img, (152, 247), (226, 321), (0, 200, 60), -1)
+        cv.rectangle(img, (152, 323), (226, 397), (0, 200, 60), -1)
+        cv.rectangle(img, (152, 399), (226, 473), (0, 200, 60), -1)
+
+        # Right
+        # Prva kolona
+        cv.rectangle(img, (484, 247), (558, 321), (255, 0, 0), -1)
+        cv.rectangle(img, (484, 323), (558, 397), (255, 0, 0), -1)
+        cv.rectangle(img, (484, 399), (558, 473), (255, 0, 0), -1)
+        # Druga kolona
+        cv.rectangle(img, (560, 247), (634, 321), (255, 0, 0), -1)
+        cv.rectangle(img, (560, 323), (634, 397), (255, 0, 0), -1)
+        cv.rectangle(img, (560, 399), (634, 473), (255, 0, 0), -1)
+        # Treca kolona
+        cv.rectangle(img, (636, 247), (710, 321), (255, 0, 0), -1)
+        cv.rectangle(img, (636, 323), (710, 397), (255, 0, 0), -1)
+        cv.rectangle(img, (636, 399), (710, 473), (255, 0, 0), -1)
+
+        # Back
+        # Prva kolona
+        cv.rectangle(img, (726, 247), (800, 321), (0, 90, 235), -1)
+        cv.rectangle(img, (726, 323), (800, 397), (0, 90, 235), -1)
+        cv.rectangle(img, (726, 399), (800, 473), (0, 90, 235), -1)
+        # Druga kolona
+        cv.rectangle(img, (802, 247), (876, 321), (0, 90, 235), -1)
+        cv.rectangle(img, (802, 323), (876, 397), (0, 90, 235), -1)
+        cv.rectangle(img, (802, 399), (876, 473), (0, 90, 235), -1)
+        # Treca kolona
+        cv.rectangle(img, (878, 247), (952, 321), (0, 90, 235), -1)
+        cv.rectangle(img, (878, 323), (952, 397), (0, 90, 235), -1)
+        cv.rectangle(img, (878, 399), (952, 473), (0, 90, 235), -1)
+
+        cv.imshow("Cube", img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+        # m = np.random.randn(3,3)
+        # img_tr = cv.transform(img, m, None)
+
 
     def print_cube(self, face):
         content = f"---|{face}|---\n"
@@ -216,7 +312,9 @@ class Cube:
 
 kocka = Cube()
 
-kocka.scramble(25)
+#kocka.scramble(25)
+
+kocka.draw_cube()
 
 #kocka.rotate_top_clockwise()       #slike 1-1 i 1-2
 #kocka.rotate_bottom_clockwise()    #slike 2-1 i 2-2
@@ -233,11 +331,11 @@ kocka.scramble(25)
 #kocka.rotate_back_counter_clockwise()
 
 
-print(kocka.print_cube("Top"))
-print(kocka.print_cube("Front"))
-print(kocka.print_cube("Left"))
-print(kocka.print_cube("Right"))
-print(kocka.print_cube("Back"))
-print(kocka.print_cube("Bottom"))
+#print(kocka.print_cube("Top"))
+#print(kocka.print_cube("Front"))
+#print(kocka.print_cube("Left"))
+#print(kocka.print_cube("Right"))
+#print(kocka.print_cube("Back"))
+#print(kocka.print_cube("Bottom"))
 
 
